18f6b63678cd7d7cd8022be90c7c56372f8780d1
AmirHossein-nr <arazlighi@gmail.com>
2024-11-22 21:49:04 +0330

    add Main for a sample usage of calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    private static double result;
+    private static String exception;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please enter the First operand: ");
+            double number1 = Double.parseDouble(scanner.nextLine());
+            System.out.println("Please enter the Second operand: ");
+            double number2 = Double.parseDouble(scanner.nextLine());
+            System.out.println("Please enter the Operation: (exit to quit)");
+            String operationStr = scanner.nextLine();
+            if (operationStr.equals("exit")) {
+                break;
+            }
+
+            switch (operationStr) {
+                case "*":
+                    result = calculator.process(number1, number2, Operations.MULTIPLY);
+                    break;
+                case "/":
+                    try {
+                        result = calculator.process(number1, number2, Operations.DIVIDE);
+                        exception = null;
+                    } catch (InvalidOperation e) {
+                        exception = e.getMessage();
+                    }
+                    break;
+                default:
+                    exception = new IllegalArgumentException("Invalid operation: " + operationStr).getMessage();
+            }
+
+            if (exception == null) {
+                System.out.println("The result is: " + result);
+            } else {
+                System.out.println("An exception occurred: " + exception);
+            }
+        }
+
+        scanner.close();
     }
 }
