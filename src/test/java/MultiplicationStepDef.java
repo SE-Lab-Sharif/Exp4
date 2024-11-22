aa8d85662640710f0ba4b34bafe71954bf0ac3b3
AmirHossein-nr <arazlighi@gmail.com>
2024-11-22 20:46:50 +0330

    add MultiplicationStepDef

---
+++ src/test/java/MultiplicationStepDef.java
@@ -0,0 +1,12 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.Before;
+
+import org.junit.Assert;
+
+import org.example.Calculator;
+
+public class MultiplicationStepDef {
+
+}
