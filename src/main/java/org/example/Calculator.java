a17ec7faaebbab5c935ccfa845e4a7df9955a4cc
AmirHossein-nr <arazlighi@gmail.com>
2024-11-22 21:09:01 +0330

    implement the Caclulator class to pass BDD tests

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class Calculator {
+    public double process(double number1, double number2, Operations operation) {
+        switch (operation) {
+            case MULTIPLY:
+                return multiply(number1, number2);
+            default:
+                throw new IllegalArgumentException("Invalid operation: " + operation);
+        }
+    }
+
+    private double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
 }
